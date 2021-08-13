0aab7478efc8a090f63ddacc8180f1942c077720
cmallen2106 <cmallen2106@gmail.com>
2021-08-13 22:24:01 +0100

    create ArrayStack and LinkedStack

--- lab7/src/lab7/Driver.java
+++ lab7/src/lab7/Driver.java
@@ -1,9 +1,25 @@
+//Christina Sosa
+//Programming Fundamentals - Summer 2021
+//Lab 7 - Exercise 1
+
+/*Exercise 1
+Create a new Driver class with a main method. In this method, do the following:
+1. Create an instance of an ArrayStack and a LinkedStack class.
+2. Push the following int's onto the two stacks: (1,7,3,4,9,2)
+3. Pop off all the elements from the stacks, displaying each int as it's removed
+*/
+
 package lab7;
+import java.util.Arrays; 
+import java.util.Stack; 
 
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// create the ArrayStack and LinkedStack
+		ArrayStack arrayStack = new ArrayStack(); 
+		LinkedStack linkedStack = new LinkedStack(); 
 
 	}
 
