32b382a587021911e8a95ac06a3552a19f2d79b8
cmallen2106 <cmallen2106@gmail.com>
2021-08-17 14:08:07 +0100

    push values into stacks and pop off elements

--- lab7/src/lab7/Driver.java
+++ lab7/src/lab7/Driver.java
@@ -20,6 +20,36 @@ public class Driver {
 		// create the ArrayStack and LinkedStack
 		ArrayStack arrayStack = new ArrayStack(); 
 		LinkedStack linkedStack = new LinkedStack(); 
+		
+		//pushing values into two stacks
+		
+		//first array stack
+		arrayStack.push(1); 
+		arrayStack.push(7); 
+		arrayStack.push(3); 
+		arrayStack.push(4); 
+		arrayStack.push(9); 
+		arrayStack.push(2); 
+		
+		//second linked stack
+		linkedStack.push(1); 
+		linkedStack.push(7); 
+		linkedStack.push(3); 
+		linkedStack.push(4);
+		linkedStack.push(9);
+		linkedStack.push(2); 
+		
+		//pop off elements from ArrayStack and display each int as it's removed
+		while (!arrayStack.isEmpty()) {
+			System.out.println("ArrayStack: " + arrayStack.pop());
+		}
+
+		System.out.println("\n");
+		
+		//pop off elements from LinkedStack and display each int as it's removed
+		while (!linkedStack.isEmpty()) {
+			System.out.println("LinkedStack: " + linkedStack.pop());
+		}
 
 	}
 
